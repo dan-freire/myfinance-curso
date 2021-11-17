2e84b94fc1a7e0961cc22e868867830db7fa76dc
Daniel Freire <dandrums.phn@gmail.com>
2021-11-17 09:23:49 -0300

    implementacao de lista de categorias

--- src/app/pages/categorias/form-categoria/form-categoria.component.ts
+++ src/app/pages/categorias/form-categoria/form-categoria.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-form-categoria',
   templateUrl: './form-categoria.component.html',
-  styleUrls: ['./form-categoria.component.sass']
+  styleUrls: ['./form-categoria.component.css']
 })
 export class FormCategoriaComponent implements OnInit {
 
--- src/app/pages/categorias/lista-categoria/lista-categoria.component.ts
+++ src/app/pages/categorias/lista-categoria/lista-categoria.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { Categoria } from '../shared/categoria.model';
+import { CategoriaService } from '../shared/categoria.service';
 
 @Component({
   selector: 'app-lista-categoria',
   templateUrl: './lista-categoria.component.html',
-  styleUrls: ['./lista-categoria.component.sass']
+  styleUrls: ['./lista-categoria.component.css']
 })
 export class ListaCategoriaComponent implements OnInit {
 
-  constructor() { }
+  categorias: Categoria[] = [];
+
+  constructor(private categoriaService: CategoriaService) { }
 
   ngOnInit() {
+    this.categoriaService.getAll()
+      .subscribe(categorias => this.categorias = categorias,
+        error => alert('Erro ao carregar a lista de categorias. ' + error))
+  }
+
+  excluirCategoria(categoria: Categoria) {
+
+    const deveExcluir = confirm('Deseja realmente excluir essa categoria?');
+
+    if (deveExcluir) {
+
+      this.categoriaService.delete(categoria.id)
+        .subscribe(
+          () => this.categorias = this.categorias.filter(elemento => elemento != categoria),
+          () => alert('Erro ao tentar excluir!'));
+    }
   }
 
 }
